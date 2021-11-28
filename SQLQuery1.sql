ad6cba0b087fbc16d4abeb192b298b7df2434569
faheemjailor <faheemjailor0055>
2021-11-28 17:11:03 +0530

    Faheem UC2 for payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,11 @@
 ---UC1 : Ability to create payroll service database
 create database payroll_services;
 use payroll_services;
+
+---UC2 : Ability create a employee payroll table in the payroll service database
+CREATE TABLE employee_payroll (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	name varchar(200),
+	salary float,
+	startDate date
+	);
